d72c00dcc5ea971cd8346ac7cbb8541eb8d2e13f
ad <adamgowland@Adams-MacBook-Pro.local>
2019-08-11 13:42:15 +0100

    Lesson 9 - basic component introduction continued

---
+++ public/lesson9/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+    <html lang="en"> 
+        <head> 
+            <meta http-equiv="content-type" content="text/html; charset=utf-8"> 
+            <title>Laracasts lesson 9</title> 
+        </head>
+
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css" />
+
+        <style>
+
+        body{
+
+            padding-top: 1em;
+
+        }
+
+        </style>
+
+    <body> 
+
+        <div id="root" class="container">
+
+            <message title="Hello World" body="Adam Gowland"></message>
+
+            <message title="Hello Everyone" body="Nicky Kurc"></message>
+
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>
+        <script src="main.js"></script>
+
+
+    </body> 
+</html>
